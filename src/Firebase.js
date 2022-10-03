91b4b942125cd7049195b2697fe86bc441c540b3
Prashant Singh <prashantsingh.999000@gmail.com>
2022-10-03 20:58:13 +0530

    added Firebase SDK and intial cleanup

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8VHfBP56UXD-SwbjGrkwpE84EXpotzFE",
+  authDomain: "netflix-clone-4f6d6.firebaseapp.com",
+  projectId: "netflix-clone-4f6d6",
+  storageBucket: "netflix-clone-4f6d6.appspot.com",
+  messagingSenderId: "64895651398",
+  appId: "1:64895651398:web:023806a4ae95ec91538447",
+  measurementId: "G-XVYM4HY909",
+};
