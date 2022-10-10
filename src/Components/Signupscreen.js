4b3bb2acf0aad262be626e48d0bed463e34726b7
Prashant Singh <prashantsingh.999000@gmail.com>
2022-10-10 18:58:37 +0530

    designed signup screen

--- src/Components/Signupscreen.js
+++ src/Components/Signupscreen.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "../Styles/Signupscreen.css";
 
 const Signupscreen = () => {
-  return <div className='signup_screen'></div>;
+  return (
+    <div className='signup_screen'>
+      <form>
+        <h1>Sign In</h1>
+        <input placeholder='Email' type='email' />
+        <input placeholder='Password' type='password' />
+        <button type='submit'>Sign In</button>
+        <h4>
+          <span className='signup_screen_gray'>New to Netflix?</span>{" "}
+          <span className='signup_screen_link'>Sign Up now.</span>
+        </h4>
+      </form>
+    </div>
+  );
 };
 
 export default Signupscreen;
